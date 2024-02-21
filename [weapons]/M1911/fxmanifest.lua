ada9f7dc533d4515eded5e8a453d0753f2824c03
Vambooze <52382335+Vambooze@users.noreply.github.com>
2024-02-21 13:34:51 -0700

    + Luxart, Takehostage, Weapons

---
+++ [weapons]/M1911/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+files{
+	'metas/**/*.meta'
+}
+
+data_file 'WEAPONINFO_FILE' 'metas/**/weapons.meta'
---
+++ [weapons]/g17/__resource.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+files{
+	'metas/**/*.meta'
+}
+data_file 'WEAPONINFO_FILE' 'metas/**/weapons.meta'
+
+client_script 'cl_weaponNames.lua'
