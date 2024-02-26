8531508d60df6d41167a04eb3e3d14927345321c
Decryption21 <austinshackleford2100@gmail.com>
2024-02-26 15:38:55 -0600

    Updated qb-radio

--- [voice]/qb-radio/config.lua
+++ [voice]/qb-radio/config.lua
@@ -3,6 +3,7 @@ Config = {}
 Config.RestrictedChannels = {
     [1] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [2] = {
@@ -11,34 +12,42 @@ Config.RestrictedChannels = {
     },
     [3] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [4] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [5] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [6] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [7] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [8] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [9] = {
         police = true,
+        bcso = true,
         ambulance = true
     },
     [10] = {
         police = true,
+        bcso = true,
         ambulance = true
     }
 }
