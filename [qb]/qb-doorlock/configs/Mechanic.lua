9677b68409d83c1b4d71b48429d9a88577f5871c
Decryption21 <austinshackleford2100@gmail.com>
2024-02-28 08:33:22 -0600

    1

---
+++ [qb]/qb-doorlock/configs/Mechanic.lua
@@ -0,0 +1,29 @@
+
+
+-- staffdoor1 created by Decryption
+Config.DoorList['Mechanic-staffdoor1'] = {
+    objName = 1289778077,
+    locked = true,
+    doorLabel = 'staff1',
+    objYaw = 93.65064239502,
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    objCoords = vec3(948.528870, -965.351929, 39.643547),
+    authorizedJobs = { ['mechanic'] = 0 },
+}
+
+-- staffdoor2 created by Decryption
+Config.DoorList['Mechanic-staffdoor2'] = {
+    objName = -626684119,
+    locked = true,
+    doorLabel = 'staff2',
+    objYaw = 183.65063476562,
+    doorType = 'door',
+    doorRate = 1.0,
+    distance = 2,
+    fixText = false,
+    objCoords = vec3(955.358276, -972.445190, 39.647919),
+    authorizedJobs = { ['mechanic'] = 0 },
+}
