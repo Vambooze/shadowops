ff6bc24d45bc7ee381d5ead08d3595858c4a21a3
Vambooze <52382335+Vambooze@users.noreply.github.com>
2024-03-09 11:50:35 -0700

    Decryption broke this and jax did too
    LOSERS

---
+++ [qb]/qb-spawn/config.lua
@@ -0,0 +1,55 @@
+QB = {}
+
+QB.Spawns = {
+    ["airport"] = {
+        coords = vector4(-1038.06, -2738.04, 20.17, 319.73),
+        location = "airport",
+        label = "Airport",
+    },
+
+    ["legion"] = {
+        coords = vector4(195.17, -933.77, 29.7, 144.5),
+        location = "legion",
+        label = "Legion Square",
+    },
+
+    ["policedp"] = {
+        coords = vector4(362.31, -1576.79, 29.29, 156.54),
+        location = "policedp",
+        jobType = "leo",
+        label = "Sheriff Station",
+    },
+
+    ["rockfordpd"] = {
+        coords = vector4(-554.31, -138.58, 38.42, 185.23),
+        location = "rockfordpd",
+        jobType = "leo",
+        label = "Rockford Police Station",
+    },
+
+    ["pillbox"] = {
+        coords = vector4(310.19, -600.55, 43.27, 261.15),
+        location = "pillbox",
+        jobType = "ems",
+        label = "Pillbox Hospital",
+    },
+
+    ["davisfire"] = {
+        coords = vector4(203.56, -1629.60, 29.64, 326.35),
+        location = "davisfire",
+        jobType = "ems",
+        label = "Davis Fire Station",
+    },
+
+    ["harmony"] = {
+        coords = vector4(1141.58, 2662.72, 38.16, 90.80),
+        location = "harmony",
+        label = "Harmony",
+    },
+
+    ["motel"] = {
+        coords = vector4(327.56, -205.08, 53.08, 163.5),
+        location = "motel",
+        label = "Motels",
+    },
+}
