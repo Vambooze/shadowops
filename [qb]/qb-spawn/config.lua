4a5721290aff1aa758523f2e7f901d1cb74d8913
Vambooze <52382335+Vambooze@users.noreply.github.com>
2024-03-10 09:50:28 -0600

    Updated qb-spawn

--- [qb]/qb-spawn/config.lua
+++ [qb]/qb-spawn/config.lua
@@ -1,6 +1,12 @@
 QB = {}
 
 QB.Spawns = {
+    ["airport"] = {
+        coords = vector4(-1038.06, -2738.04, 20.17, 319.73),
+        location = "airport",
+        label = "Airport",
+    },
+
     ["legion"] = {
         coords = vector4(195.17, -933.77, 29.7, 144.5),
         location = "legion",
@@ -8,20 +14,30 @@ QB.Spawns = {
     },
 
     ["policedp"] = {
-        coords = vector4(428.23, -984.28, 29.76, 3.5),
+        coords = vector4(362.31, -1576.79, 29.29, 156.54),
         location = "policedp",
-        label = "Police Department",
+        jobType = "leo",
+        label = "Sheriff Station",
     },
 
-    ["paleto"] = {
-        coords = vector4(80.35, 6424.12, 31.67, 45.5),
-        location = "paleto",
-        label = "Paleto Bay",
+    ["pillbox"] = {
+        coords = vector4(310.19, -600.55, 43.27, 261.15),
+        location = "pillbox",
+        jobType = "ems",
+        label = "Pillbox Hospital",
     },
 
-    ["motel"] = {
-        coords = vector4(327.56, -205.08, 53.08, 163.5),
-        location = "motel",
-        label = "Motels",
+    ["davisfire"] = {
+        coords = vector4(203.56, -1629.60, 29.64, 326.35),
+        location = "davisfire",
+        jobType = "ems",
+        label = "Davis Fire Station",
     },
+
+    ["harmony"] = {
+        coords = vector4(1141.58, 2662.72, 38.16, 90.80),
+        location = "harmony",
+        label = "Harmony",
+    },
+
 }
