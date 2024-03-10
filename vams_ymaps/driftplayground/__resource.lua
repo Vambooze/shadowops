243dc72c68890754ebe0ac6462705b304b5c94b7
Vambooze <52382335+Vambooze@users.noreply.github.com>
2024-03-09 18:04:50 -0700

    Added nicks gay map

---
+++ vams_ymaps/driftplayground/__resource.lua
@@ -0,0 +1,10 @@
+data_file 'GTXD_PARENTING_DATA' 'meta/gtxd.meta' 
+file 'meta/gtxd.meta'
+
+this_is_a_map 'yes'
+client_script 'client.lua'
+data_file 'DLC_ITYP_REQUEST' 'driftplayground.ytyp'
+
+
+
+
