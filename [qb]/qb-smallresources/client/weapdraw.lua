b99165cd51ecd1359142113e0b926457ea682767
Vambooze <52382335+Vambooze@users.noreply.github.com>
2024-03-10 17:08:23 -0600

    added no prop fall when damaged

--- [qb]/qb-smallresources/client/weapdraw.lua
+++ [qb]/qb-smallresources/client/weapdraw.lua
@@ -336,3 +336,13 @@ function CeaseFire()
         end
     end)
 end
+
+Citizen.CreateThread(function()
+    while true do
+        if PlayerPedId() ~= lastped then
+            lastped = PlayerPedId()
+            SetPedCanLosePropsOnDamage(PlayerPedId(), false, 0)
+        end
+        Wait(100)
+    end
+end)
